ea8bbcaa96f6c8c6e7677aca4f63758a50c9600d
Dennis Weispfennig <uk061572@student.uni-kassel.de>
2023-12-04 19:26:11 +0100

    finished task 1

---
+++ exercises/dennis/exercise_2/exercise_2.py
@@ -0,0 +1,13 @@
+import pandapower as pp
+import pandapower.networks as nw
+
+mv_oberrhein = nw.mv_oberrhein(scenario="generation")
+
+# scaling factors before update
+scaling_before = mv_oberrhein.sgen["p_mw"]
+print(mv_oberrhein.sgen.loc[0:5,["p_mw","q_mvar"]])
+
+# updating the real power to increse the real power by a factor of 3.4
+
+mv_oberrhein.sgen.loc[:,"p_mw"] = mv_oberrhein.sgen.loc[:,"p_mw"] * 3.4
+print(mv_oberrhein.sgen.loc[0:5,["p_mw","q_mvar"]])
