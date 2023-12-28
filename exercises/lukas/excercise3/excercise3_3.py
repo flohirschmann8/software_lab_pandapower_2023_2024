d6968b583917d7209182a783da877658efc71d47
Lukas Kramer <uk070332@student.uni-kassel.de>
2023-12-28 12:53:51 +0100

    Update Excercise 4.1, 4.2, 4.4

---
+++ exercises/lukas/excercise3/excercise3_3.py
@@ -0,0 +1,60 @@
+import pandapower as pp
+import pandapower.networks as nw
+import pandas as pd
+import copy
+import matplotlib.pyplot as plt
+
+
+
+iterations = 100
+results = pd.DataFrame(columns=["installed", "violation"])
+
+#Funktion get_plant_size_mw gibt einen zufälligen Wert aus einer Normalverteilung zurück (Mittelwert: 0.5, Abweichung 0.05)
+from numpy.random import normal
+def get_plant_size_mw():
+    return normal(loc=0.5, scale=0.05)
+
+#Funktion chose_bus(net) wählt einen zufälligen Knoten des Netzwerks aus
+from numpy.random import choice
+def chose_bus(net):
+    return choice(net.load.bus.values)
+#Definition von Grenzwertverletzungen (hier >90% Auslastung bzw. Spannungspegel >106%
+def violations(net):
+    pp.runpp(net)
+    if net.res_line.loading_percent.max() > 90:
+        return (True, "Line \n Overloading")
+    elif net.res_trafo.loading_percent.max() > 90:
+        return (True, "Transformer \n Overloading")
+    elif net.res_bus.vm_pu.max() > 1.06:
+        return (True, "Voltage \n Violation")
+    else:
+        return (False, None)
+
+def load_network():
+    return nw.mv_oberrhein(scenario="generation")
+
+for i in range(iterations):
+    net = load_network()
+    installed_mw = 0
+    while 1:
+        violated, violation_type = violations(net)
+        if violated:
+            results.loc[i] = [installed_mw, violation_type]
+            break
+        else:
+            plant_size = get_plant_size_mw()
+            pp.create_sgen(net, chose_bus(net), p_mw=plant_size, q_mvar=0)
+            installed_mw += plant_size
+            net_copy = copy.deepcopy(net)
+
+summation = sum(results.installed)
+arithmetic_mean = summation / iterations
+print(arithmetic_mean)
+
+data = {"Measures":["Leitungen ersetzen", "Power-to-Gas Lasten anschließen"],
+        "Costs": [55, 330000]}
+df_expansion = pd.DataFrame(data)
+
+
+
+
