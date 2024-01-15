e6f8b37e74cfa165cb73ac0638ab6f90d95d462d
hamadabdalrhman <uk087243@student.uni-kassel.de>
2024-01-15 04:47:15 +0100

    I sketeched the grid area 4, and assigned colors for buses and lines, I also specified the number of all assets for my grid area 4 at the sketch legend

--- exercises/abdalrhman/exercise_4/exercise_4_I_1.py
+++ exercises/abdalrhman/exercise_4/exercise_4_I_1.py
@@ -63,18 +63,51 @@ idx_ext_grids = net.ext_grid.index[net.ext_grid.bus.isin(idx_buses)]
 n_of_ext_grids = len(idx_ext_grids)
 
 
+### TODO: reminder to summurize the grid description in word
+
+###### grid sketches #######
+
+### assigning the lines and buses groups that distingush between area 4 and the other areas
+lc_area4  = plot.create_line_collection(net, idx_lines, zorder=2, linewidths=2 , color = 'green')
+lc_others = plot.create_line_collection(net, net.line.index[~net.line.index.isin(idx_lines)],color = 'grey')
+
+bc_area4 = plot.create_bus_collection(net, idx_buses, size=80, zorder=2, color='blue')
+bc_others = plot.create_bus_collection(net, net.bus.index[~net.bus.index.isin(idx_buses)], size=80, zorder=2,color = 'grey')
+
+plot.draw_collections(collections=[lc_area4, lc_others,bc_area4,bc_others], figsize=(12, 8))
+
+## draw legends for grid assets
+legend_labels = [f'{n_of_busus} Buses at area 4',
+                 f'{n_of_lines} Lines at area 4',
+
+                 f'{n_of_switches} Switches at area',
+                 f'{n_of_sgens} Sgen at area 4',
+                 f'{n_of_loads} Loads at area 4',
+                 f'{n_of_trafos} Trafo at area 4',
+                 f'{n_of_ext_grids} External grid at area 4',
+
+                 f'{len(net.line.index[~net.line.index.isin(idx_lines)])} Lines at other areas',
+                 f'{len(net.bus.index[~net.bus.index.isin(idx_buses)])} Buses at other areas']
+legend_handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10),
+                  plt.Line2D([0], [0], color='green', linewidth=2),
+
+                  plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10),
+                  plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10),
+                  plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10),
+                  plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10),
+                  plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10),
+
+                  plt.Line2D([0], [0], color='grey', linewidth=2),
+                  plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='grey', markersize=10)]
 
 
 
+plt.legend(legend_handles, legend_labels, loc='upper right')
 
 
-### Grid area 4 consist of 45 buses,
+plt.show()
 
+# plt.savefig(r".\exercises\abdalrhman\exercise_4\net_exercise_4_I_1.png")
 
-
-
-
-
-
-
+## TODO: put the sketch on word
 
