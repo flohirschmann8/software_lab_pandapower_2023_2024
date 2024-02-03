f1e10e2b565b5e4bd492220014f24b7c0708f51f
alikemal6496 <uk037988@student.uni-kassel.de>
2024-02-03 19:48:12 +0100

    loading network from json file and splitting network

---
+++ exercises/Ali_Kemal/exercise4/fc_dataoutput_subnet.py
@@ -0,0 +1,40 @@
+import exercise_4
+
+
+
+
+
+
+def data_output_subnet(
+    #Printing number of lines, buses, loads feed-in plants in subnetwork
+    # Print number of buses
+    nr_buses = len(buses_area2)
+    print("The network includes ", nr_buses, "buses.")
+
+    # Print number of lines
+    nr_lines = len(buses_area2)
+    print("The network includes ", nr_lines, "lines.")
+
+
+    # Print number of loads
+    nr_loads = []
+    x = buses_area2[0]
+
+    for x in range(buses_area2[0], buses_area2[-1]):
+        nr_loads.append(net.load[net.load.bus == x])
+        x += 1
+
+    print("The network includes ", len(nr_loads), "loads.")
+
+    # Print number of gens
+    nr_gens = []
+    x = buses_area2[0]
+    for x in range(buses_area2[0], buses_area2[-1]):
+        nr_gens.append(net.sgen[net.sgen.bus == x])
+        x += 1
+
+    print("The network includes ", len(nr_gens), "gens.")
+)
+
+
+
