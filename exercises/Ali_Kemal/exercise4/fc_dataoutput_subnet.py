165036c4a44815e9688bb86a3fb9c0cf33972302
alikemal6496 <uk037988@student.uni-kassel.de>
2024-02-04 09:22:40 +0100

    loading network from json file and splitting network

--- exercises/Ali_Kemal/exercise4/fc_dataoutput_subnet.py
+++ exercises/Ali_Kemal/exercise4/fc_dataoutput_subnet.py
@@ -1,38 +1,34 @@
-import exercise
 
-
-
-
-def data_output_subnet(
+def data_output_subnet(buses_subnet, network):
     #Printing number of lines, buses, loads feed-in plants in subnetwork
     # Print number of buses
-    nr_buses = len(buses_area2)
+    nr_buses = len(buses_subnet)
     print("The network includes ", nr_buses, "buses.")
 
     # Print number of lines
-    nr_lines = len(buses_area2)
+    nr_lines = len(buses_subnet)
     print("The network includes ", nr_lines, "lines.")
 
 
     # Print number of loads
     nr_loads = []
-    x = buses_area2[0]
+    x = buses_subnet[0]
 
-    for x in range(buses_area2[0], buses_area2[-1]):
-        nr_loads.append(net.load[net.load.bus == x])
+    for x in range(buses_subnet[0], buses_subnet[-1]):
+        nr_loads.append(network.load[network.load.bus == x])
         x += 1
 
     print("The network includes ", len(nr_loads), "loads.")
 
     # Print number of gens
     nr_gens = []
-    x = buses_area2[0]
-    for x in range(buses_area2[0], buses_area2[-1]):
-        nr_gens.append(net.sgen[net.sgen.bus == x])
+    x = buses_subnet[0]
+    for x in range(buses_subnet[0], buses_subnet[-1]):
+        nr_gens.append(network.sgen[network.sgen.bus == x])
         x += 1
 
     print("The network includes ", len(nr_gens), "gens.")
-)
+
 
 
 
