0385a1ec8924f94be3e106813099ba8727f297fb
Kai-luca Ponzer <uk069509@student.uni-kassel.de>
2024-02-09 15:17:00 +0100

    exercise_5_update

--- exercises/Kai-Luca/exercise_4/exercise_5.py
+++ exercises/Kai-Luca/exercise_4/exercise_5.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import pandas as pd
+import pandapower.toolbox as tool
 from pandapower.timeseries import DFData
 from pandapower.timeseries import OutputWriter
 from pandapower.timeseries.run_time_series import run_timeseries
@@ -28,49 +29,44 @@ def area3_net():
     net = pp.from_json("C:/Users/User/Documents/Uni/Master/1. Semester/Pandapower/Hausarbeit/Netz/net_exercise_4.json")
     mg = top.create_nxgraph(net)
     buses_area3 = list(top.connected_component(mg, bus=89))
-    return net, buses_area3
+    lines_area3 = list(tool.get_connected_elements(net=net, element_type="line", buses=buses_area3))
+    loads_area3 = list(tool.get_connected_elements(net=net, element_type="load", buses=buses_area3))
+    sgens_area3 = list(tool.get_connected_elements(net=net, element_type="sgen", buses=buses_area3))
+    return net, buses_area3, lines_area3, loads_area3, sgens_area3
 
 
-
-def create_controllers(net, buses_area3, ds):
-    loads_area3 = net.load.bus.isin(buses_area3)
-    sgen_area3 = net.sgen.bus.isin(buses_area3)
-    loads = pd.DataFrame(net.load[loads_area3]).index
-    sgens = pd.DataFrame(net.sgen[sgen_area3]).index
-    ConstControl(net, element="load", variable="scaling", element_index=loads,
+def create_controllers(net, loads_area3, sgens_area3, ds):
+    ConstControl(net, element="load", variable="scaling", element_index=loads_area3,
                  data_source=ds, profile_name="loads")
-    ConstControl(net, element="sgen", variable="scaling", element_index=sgens,
+    ConstControl(net, element="sgen", variable="scaling", element_index=sgens_area3,
                  data_source=ds, profile_name="sgens")
+
     KLController(net, max_limit_pu=1.05, min_limit_pu=0.95)
-    dennis_net = MyGrid(net, 0)
-    W_Controller(net, element="line_loading",my_grid=dennis_net,limits_pu=[1.0])
+    dennis_net = MyGrid(net, 2)
+    W_Controller(net, element="voltage", my_grid=dennis_net,limits_pu=[0.95, 1.05])
     return net
 
 
 def create_output_writer(net, buses_area3, time_steps, output_dir):
     ow = OutputWriter(net, time_steps, output_path=output_dir, output_file_type=".xlsx", log_variables=list())
-    mask_hv_buses_area3 = net.bus.loc[buses_area3]
-    mask_lines_area3 = net.line[(net.line.from_bus.isin(buses_area3)) & net.line.to_bus.isin(buses_area3)].index
 
-    ow.log_variable("res_line", "loading_percent", index=mask_lines_area3, eval_function=np.max, eval_name="Max. Leitungsauslastung")
-    ow.log_variable("res_bus", "vm_pu", index=mask_hv_buses_area3, eval_function=np.max, eval_name="Max. Spannungspegel")
-    ow.log_variable("res_bus", "vm_pu", index=mask_hv_buses_area3, eval_function=np.min, eval_name="Min. Spannungspegel")
+    ow.log_variable("res_bus", "vm_pu", index=buses_area3, eval_function=np.max, eval_name="Max. Spannungspegel")
+    ow.log_variable("res_bus", "vm_pu", index=buses_area3, eval_function=np.min, eval_name="Min. Spannungspegel")
 
-    dennis_net = MyGrid(net, 0)
-    ow.log_variable("res_line", "loading_percent", index=dennis_net.get_indices("line"), eval_function=np.max, eval_name="Max. Leitungsauslastung Dennis")
+    dennis_net = MyGrid(net, 2)
+    #ow.log_variable("res_line", "loading_percent", index=dennis_net.get_indices("line"), eval_function=np.max, eval_name="Max. Leitungsauslastung Dennis")
     ow.log_variable("res_bus", "vm_pu", index=dennis_net.get_indices("bus"), eval_function=np.max, eval_name="Max. Spannungspegel Dennis")
     ow.log_variable("res_bus", "vm_pu", index=dennis_net.get_indices("bus"), eval_function=np.min, eval_name="Min. Spannungspegel Dennis")
 def timeseries_area3(output_dir):
-    net, buses_area3 = area3_net()
-    # n_timesteps = len(data.loads)
+    net, buses_area3, lines_area3, loads_area3, sgens_area3 = area3_net()
     profiles, ds, n_timesteps = create_data_source()
-    create_controllers(net, buses_area3, ds)
+    create_controllers(net, loads_area3, sgens_area3, ds)
     time_steps = range(0, n_timesteps)
-    create_output_writer(net, buses_area3, time_steps, output_dir=output_dir)
+    create_output_writer(net, buses_area3, time_steps, output_dir)
     run_timeseries(net, time_steps)
 
 
-output_dir = r"C:\Users\User\Documents\Uni\Master\1. Semester\Pandapower\Hausarbeit\Zeitreihen_4.4\Output dir"
+output_dir = r"C:\Users\User\Documents\Uni\Master\1. Semester\Pandapower\Hausarbeit\Zeitreihen_4.5\Output dir"
 print("Results can be found in your Zeitreihen folder: {}".format(output_dir))
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
@@ -79,11 +75,12 @@ timeseries_area3(output_dir)
 vm_pu_file = os.path.join(output_dir, "res_bus", "vm_pu.xlsx")
 vm_pu = pd.read_excel(vm_pu_file, index_col=0)
 vm_pu.plot(label="vm_pu")
-plt.hlines(y=1.05, xmin=0, xmax=len(vm_pu))
-plt.hlines(y=0.95, xmin=0, xmax=len(vm_pu), color="orange")
-plt.xlabel("time step")
+plt.hlines(y=1.05, xmin=0, xmax=len(vm_pu), linestyles="dashed", label="max. limit", color="red")
+plt.hlines(y=0.95, xmin=0, xmax=len(vm_pu), linestyles="dashed", color="red", label="min. limit")
+plt.xlabel("Zeitschritt")
 plt.ylabel("Spannungspegel [p.u.]")
 plt.title("Spannungspegel")
+plt.legend()
 plt.grid()
 plt.show()
 
